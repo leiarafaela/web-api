627ac64667727ed7809bbced35785b0fa6809672
Rafaela Augusto S <rafaela.augusto@somei.com.vc>
2022-04-18 10:36:22 -0300

    add methodos

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Models;
 using WebApi.Services;
 
 namespace WebApi.Controllers
-{   
+{
     [ApiController]
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
@@ -22,16 +18,55 @@ namespace WebApi.Controllers
             await _productsService.GetAsync();
 
 
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Product>> Get(string id)
+        {
+            var product = await _productsService.GetAsync(id);
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+
         [HttpPost]
         public async Task<IActionResult> Post(Product newProduct)
         {
             await _productsService.CreateAsync(newProduct);
 
-            return CreatedAtAction(nameof(Get), new { id = newProduct.Id}, newProduct);
+            return CreatedAtAction(nameof(Get), new { id = newProduct.Id }, newProduct);
 
         }
-            
-        
-        
+
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, Product updateProduct)
+        {
+            var product = await _productsService.GetAsync(id);
+            if (product is null){
+                return NotFound();
+            }
+            updateProduct.Id = product.Id;
+
+            await _productsService.UpdateAsync(id, updateProduct);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var product = await _productsService.GetAsync(id);
+            if (product is null)
+            {
+                return NotFound();
+            }
+            await _productsService.RemoveAsync(id);
+
+            return NoContent();  
+        }
+
     }
 }
